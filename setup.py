1f4b31f7075d48f5c9c75822ddca11e072a85353
Ebenezer319 <42072200+Ebenezer319@users.noreply.github.com>
2021-07-28 14:38:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import pathlib
+
+
+
+def readme():
+    this_directory = pathlib.Path(__file__).parent.resolve()
+    long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
+    
+    return long_description
+
+
+setup(name='csv_pull',
+      version='1.0.0',
+      description='Data Extract',
+      long_description=readme(),
+      long_description_content_type = 'text/markdown',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Topic :: CSV Extraction :: File Manipulation',
+      ],
+      url='https://github.com/Ebenezer319/csv_pull',
+      author='CSV Extractors',
+      packages=['csv_pull'],
+      install_requires=[
+          'numpy',
+          'pandas'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['pull-csv=csv_pull.csv_pull:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)
